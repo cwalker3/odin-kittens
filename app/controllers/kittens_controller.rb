76cd585a78239477c33daf60925108c4f1038312
Cory Walker <cory.walker.dev@gmail.com>
2023-01-29 18:55:48 -0800

    Add Kitten model, controller, and views

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,49 @@
+class KittensController < ApplicationController
+  before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to @kitten, notice: 'Successfully created kitten'
+    else
+      render :new, status: :unprocessable_entity, alert: 'Improper kitten parameters!'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity, alert: 'Improper kitten parameters!'
+    end
+  end
+
+  def destroy
+    @kitten.destroy
+  end
+
+  def show
+  end
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  private
+
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
